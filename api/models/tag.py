575101e177443da2570e545fd6aef56d5df05527
boo <booblegum42@gmail.com>
2022-12-10 01:28:23 +0700

    Handlers and Swagger docs for Tag

--- api/models/tag.py
+++ api/models/tag.py
@@ -1,7 +1,20 @@
 from api import db
+from sqlalchemy.exc import IntegrityError
 
 
 class TagModel(db.Model):
     __tablename__ = 'tag'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
+
+    def save(self):
+        # try:
+        db.session.add(self)
+        db.session.commit()
+        # except IntegrityError as e:
+        #     db.session.rollback(e)
+        #     raise IntegrityError
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from api import app, docs
 from config import Config
-from api.handlers import auth, note, user
+from api.handlers import auth, note, user, tag
 
 # CRUD
 
@@ -16,5 +16,12 @@ docs.register(user.create_user)
 
 # NOTES
 docs.register(note.get_note_by_id)
+
+# TAGS
+docs.register(tag.get_tag_by_id)
+docs.register(tag.get_tags)
+docs.register(tag.create_tag)
+docs.register(tag.edit_tag)
+docs.register(tag.delete_tag)
 if __name__ == '__main__':
     app.run(debug=Config.DEBUG, port=Config.PORT)
